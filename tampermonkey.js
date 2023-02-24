1eb358da10e2f281d182addd3a4bed2b35b4e2a2
Kenny Chou <kennyc015@gmail.com>
2023-02-24 17:46:15 +0800

    update match url

--- tampermonkey.js
+++ tampermonkey.js
@@ -2,7 +2,7 @@
 // @name         __APP_NAME__
 // @namespace    https://kennychou.github.io/
 // @version      __APP_VERSION__
-// @description  使用Vue2.x構建Tampermonkey for ebird checklist
+// @description  eBird GPS Track Download
 // @author       Kenny Chou
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -17,4 +17,5 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js
 // @require      https://unpkg.com/xmlbuilder2
 // @match        https://ebird.org/checklist/*
+// @match        https://ebird.org/*/checklist/*
 // ==/UserScript==
